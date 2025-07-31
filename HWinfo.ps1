1dc6b76f5f62ea6ff5b9a58abbd36a9d113fbd23
Stepanov Vladislav <116786957+vladislst@users.noreply.github.com>
2025-07-31 15:44:23 +0500

    Add files via upload

---
+++ HWinfo.ps1
@@ -0,0 +1,18 @@
+﻿$NamePC=$args[0];
+Get-CimInstance -ClassName Win32_ComputerSystem -comp $NamePC | fl Manufacturer,Model;
+sleep 2;
+echo "CPU";
+Get-CimInstance -ClassName Win32_Processor -comp $NamePC | fl;
+sleep 2;
+echo "RAM";
+Get-CimInstance -ClassName Win32_PhysicalMemory -comp $NamePC | fl DeviceLocator,BankLabel,Manufacturer,PartNumber,Speed,FormFactor,@{label="Capacity (GB)"; Expression={$_.Capacity / 1gb}};
+sleep 2;
+echo "HDD";
+Get-CimInstance -ClassName Win32_DiskDrive -comp $NamePC | fl MediaType,Model,@{label="Size";Expression={$_.Size / 1gb}};
+sleep 2;
+echo "Видеокарта";
+Get-CimInstance -ClassName Win32_VideoController -comp $NamePC | fl Name,VideoProcessor,Status
+sleep 2;
+echo "Монитор"; 
+gwmi -comp $NamePC WmiMonitorID -Namespace root\wmi | ForEach-Object {($_.UserFriendlyName -ne 0 | foreach {[char]$_}) -join "";};
+
